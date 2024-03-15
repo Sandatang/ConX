a0e8d2b879cddc4d3dce44f9467d4bd6025d8c50
Sandatang <mushi.elite@gmail.com>
2024-03-15 18:38:43 +0800

    Added tags property

---
+++ backend/CONX/Migrations/20240315031930_AddedTagsForum.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CONX.Migrations
+{
+    public partial class AddedTagsForum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Forums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Forums");
+        }
+    }
+}
