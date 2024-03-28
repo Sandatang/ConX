8389427b4339505b49cb7463410404b0b5bd950d
Shaireen Alexis Moncada <shaireenalexismoncada@gmail.com>
2024-03-28 10:17:38 +0800

    most popular + workshop model

---
+++ backend/CONX/Models/Workshop.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CONX.Models
+{
+    public class Workshop
+    {
+        public int  WorkshopId { get; set; }
+        public string WorkshopTitle { get; set; }
+        [ForeignKey("UserId")]
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+        public string Tags {  get; set; } //separated by comma
+        public string Description { get; set; }
+        public int LikesCount { get; set; } 
+
+
+    }
+}
