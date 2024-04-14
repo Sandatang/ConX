fc6fd447451c86811bcc2f997ca343977a3fef96
Sandatang <mushi.elite@gmail.com>
2024-04-14 15:16:35 +0800

    Added BulletinApi ADD and RETRIEVE

---
+++ backend/CONX/Models/BulletinPost.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CONX.Models
+{
+    public class BulletinPost
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("UserId")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public string PostTitle { get; set; }
+        public string PostBody { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string? ImgUrl { get; set; }
+    }
+}
