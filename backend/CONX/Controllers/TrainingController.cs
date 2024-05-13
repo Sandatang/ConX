2e8a8a9a7094b2bb242afc5d4fdb07af464f0d4b
Sandatang <mushi.elite@gmail.com>
2024-05-13 18:02:34 +0800

    Added Registered Users For a Training

--- backend/CONX/Controllers/TrainingController.cs
+++ backend/CONX/Controllers/TrainingController.cs
@@ -1,5 +1,6 @@
 ﻿using CONX.Models;
 using CONX.Models.TrainingViewModels;
+using ConXUser.Management.Service.Model;
 using ConXUser.Management.Service.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +14,14 @@ namespace CONX.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly IConfiguration _configuration;
+        private readonly IEmailServices _emailService;
         private readonly ApplicationDbContext _context;
 
-        public TrainingController(UserManager<IdentityUser> userManager, IConfiguration configuration, ApplicationDbContext context)
+        public TrainingController(UserManager<IdentityUser> userManager, IConfiguration configuration, IEmailServices emailService, ApplicationDbContext context)
         {
             _userManager = userManager;
             _configuration = configuration;
+            _emailService = emailService;
             _context = context;
         }
 
@@ -94,6 +97,107 @@ namespace CONX.Controllers
             var trainings = await _context.Trainings.ToListAsync();
 
             return Ok(trainings);
+        }
+
+        [HttpGet]
+        [Route("getOne/{id}")]
+        public async Task<IActionResult> GetOneTraining(string id)
+        {
+            var training = await _context.Trainings.FindAsync(Int32.Parse(id));
+            if (training == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                     new Response { Status = "Error", Message = "Training not exist", Field = "failed" });
+            }
+
+            return Ok(training);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> DeleteTraining(string id)
+        {
+            var training = await _context.Trainings.FindAsync(Int32.Parse(id));
+            if (training == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                     new Response { Status = "Error", Message = "Training not exist", Field = "failed" });
+            }
+
+            _context.Trainings.Remove(training);
+
+            var result = await _context.SaveChangesAsync();
+
+            if (result <= 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                   new Response { Status = "Error", Message = "Something went wrong try again later", Field = "failed" });
+            }
+            return Ok(new Response { Status = "Success", Message = "Training removed successfully"});
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterTraining([FromBody] RegisterTraining registerTraining)
+        {
+            var training = await _context.Trainings.FindAsync(registerTraining.TrainingId);
+            if(training == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                     new Response { Status = "Error", Message = "Training not exist", Field = "failed" });
+            }
+
+            var user = await _userManager.FindByIdAsync(registerTraining.UserId);
+
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound,
+                    new Response { Status = "Error", Message = "User not exist", Field = "failed" });
+            }
+
+            var regTraining = new TrainingRegistration
+            {
+                UserId = registerTraining.UserId,
+                TrainingId = registerTraining.TrainingId,
+                Email = registerTraining.Email,
+                ContactNo  = registerTraining.ContactNo,
+            };
+
+            _context.TrainingRegistrations.Add(regTraining);
+            var result = await _context.SaveChangesAsync();
+
+            if(result <= 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                   new Response { Status = "Error", Message = "Something went wrong try again later", Field = "failed" });
+            }
+
+            var messageContent = $"Thank you for registering for {training.TrainingName}!";
+            var message = new Message(new string[] { registerTraining.Email! }, "Event Registration Confirmation", messageContent);
+            _emailService.SendEmail(message);
+
+            return Ok(new Response { Status = "Success", Message = "Thank you for registering. Check your email for details" }); 
+        }
+
+        [HttpGet]
+        [Route("getRegistered")]
+        public async Task<IActionResult> GetAllRegistered()
+        {
+            var registered = await _context.TrainingRegistrations
+                                                                .Include(tr => tr.User) 
+                                                                .Include(tr => tr.Training) 
+                                                                .Select(tr => new
+                                                                {
+                                                                    UserId = tr.UserId,
+                                                                    UserFirstname = tr.User.Firstname,
+                                                                    UserMiddlename = tr.User.Middlename,
+                                                                    UserLastname = tr.User.Lastname,
+                                                                    TrainingId = tr.TrainingId,
+                                                                    TrainingTitle = tr.Training.TrainingName,
+                                                                    TrainingDescription = tr.Training.TrainingDescription,
+                                                                })
+                                                                .ToListAsync();
+            return Ok(registered);
         }
     }
 }
---
+++ backend/CONX/Models/TrainingViewModels/RegisterTraining.cs
@@ -0,0 +1,10 @@
+﻿namespace CONX.Models.TrainingViewModels
+{
+    public class RegisterTraining
+    {
+        public int TrainingId { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
